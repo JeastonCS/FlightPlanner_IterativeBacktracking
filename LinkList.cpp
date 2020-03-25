8814c25681432fd3cad2bbbdb4e95fda0ad25ac1
Jeaston <54759351+Jeaston@users.noreply.github.com>
2020-03-25 13:54:52 -0500

    finished LinkList no parameter constructor and destructor

--- LinkList.cpp
+++ LinkList.cpp
@@ -2,4 +2,18 @@
 // Created by Jack Easton on 3/25/2020.
 //
 
+#include "ListNode.h"
 #include "LinkList.h"
+
+template <typename T>
+LinkList<T>::LinkList() : size(0), head(nullptr), tail(nullptr) {}
+
+template <typename T>
+LinkList<T>::~LinkList() {
+    ListNode<T> *curr = head;
+    while (curr != nullptr) {
+        head = curr->getNext();
+        delete curr;
+        curr = head;
+    }
+}
--- LinkList.h
+++ LinkList.h
@@ -5,10 +5,31 @@
 #ifndef S20_PA04_FLIGHTPLANNER_LINKLIST_H
 #define S20_PA04_FLIGHTPLANNER_LINKLIST_H
 
+#include "ListNode.h"
 
+template <typename T>
 class LinkList {
+private:
+    ListNode<T> *head, *tail;
+    int size;
+public:
+    LinkList();
+    ~LinkList();
+
+    LinkList & operator= (const LinkList &);
+
+
+    void push_front(ListNode<T> &);
+    void push_back(ListNode<T> &);
+    ListNode<T> & pop_front();
+    ListNode<T> & pop_back();
+    bool isEmpty();
+    bool contains(const ListNode<T> &);
+
+    int getSize();
+    ListNode<T> & getFront();
+    ListNode<T> & getBack();
 
 };
-
 
 #endif //S20_PA04_FLIGHTPLANNER_LINKLIST_H
